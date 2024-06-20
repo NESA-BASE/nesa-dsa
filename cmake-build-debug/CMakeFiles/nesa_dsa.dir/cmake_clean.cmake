0d9c75cec2d47363276b1aa50a02afa9fd9e1b67
Bellamy01 <bdanbellamy@gmail.com>
2024-06-20 23:34:39 +0200

    _

---
+++ cmake-build-debug/CMakeFiles/nesa_dsa.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nesa_dsa.dir/main.cpp.o"
+  "CMakeFiles/nesa_dsa.dir/main.cpp.o.d"
+  "nesa_dsa"
+  "nesa_dsa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nesa_dsa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <fstream>
 #include <sstream>
 #include <limits>
 #include <algorithm>
 #include <cctype>
-#include <bits/stdc++.h>
 using namespace std;
 
 struct Patient {
@@ -43,30 +41,6 @@ struct Appointment {
 
 };
 
-template<typename EnumType, EnumType... Values>
-class EnumCheck;
-
-template<typename EnumType>
-class EnumCheck<EnumType>
-{
-public:
-    template<typename IntType>
-    static bool constexpr is_value(IntType) { return false; }
-};
-
-template<typename EnumType, EnumType V, EnumType... Next>
-class EnumCheck<EnumType, V, Next...> : private EnumCheck<EnumType, Next...>
-{
-    using super = EnumCheck<EnumType, Next...>;
-
-public:
-    template<typename IntType>
-    static bool constexpr is_value(IntType v)
-    {
-        return v == static_cast<IntType>(V) || super::is_value(v);
-    }
-};
-
 class PatientLL {
     Patient* head;
 
@@ -81,33 +55,12 @@ public:
         head = newPatient;
     }
 
-    // Method to find a Patient by ID
-    Patient* findPatient(int id) {
-        Patient* current = head;
-        while (current != nullptr) {
-            if (current->patient_id == id)
-                return current;
-            current = current->next;
-        }
-        return nullptr;
-    }
-
     // Method to display all Patients
     void displayPatients() {
         Patient* current = head;
         while (current != nullptr) {
             cout << "Patient ID: " << current->patient_id << ", Name: " << current->name << ", DoB: " << current->dob << ", Gender: " << current->gender << endl;
             current = current->next;
-        }
-    }
-
-    // Method to search for a Patient by ID and display details
-    void searchPatient(int id) {
-        Patient* patient = findPatient(id);
-        if (patient != nullptr) {
-            cout << "Patient ID: " << patient->patient_id << ", Name: " << patient->name << ", DoB: " << patient->dob << ", Gender: " << patient->gender << endl;
-        } else {
-            cout << "Patient with ID " << id << " not found." << endl;
         }
     }
 
@@ -152,16 +105,6 @@ public:
         }
     }
 
-    // Method to search for a Doctor by ID and display details
-    void searchDoctor(int id) {
-        Doctor* doctor = findDoctor(id);
-        if (doctor != nullptr) {
-            cout << "Doctor ID: " << doctor->doctor_id << ", Name: " << doctor->name << ", Specialization: " << doctor->specialization << endl;
-        } else {
-            cout << "Doctor with ID " << id << " not found." << endl;
-        }
-    }
-
     // Getter for the head of the Doctor list
     Doctor* getHead() const {
         return head;
@@ -182,17 +125,6 @@ public:
         head = newAppointment;
     }
 
-    // Method to find an Appointment by ID
-    Appointment* findAppointment(int id) {
-        Appointment* current = head;
-        while (current != nullptr) {
-            if (current->appointment_id == id)
-                return current;
-            current = current->next;
-        }
-        return nullptr;
-    }
-
     // Method to display all Appointments
     void displayAppointments() {
         Appointment* current = head;
@@ -203,13 +135,17 @@ public:
     }
 
     // Method to search for a Appointment by ID and display details
-    void searchAppointment(int id) {
-        Appointment* appointment = findAppointment(id);
-        if (appointment != nullptr) {
-            cout << "Appointment ID: " << appointment->appointment_id << ", Patient ID: " << appointment->patient_id << ", Doctor ID: " << appointment->doctor_id << ", Appointment Date: " << appointment->appointment_date << endl;
-        } else {
-            cout << "Appointment with ID " << id << " not found." << endl;
+    Appointment* findAppointmentByDate(int patient_id, int doctor_id, string appointment_date) {
+        Appointment* current = head;
+
+        while (current != nullptr) {
+            if (current->patient_id == patient_id && current->doctor_id == doctor_id && current->appointment_date == appointment_date) {
+                return current;
+            }
+            current = current->next;
         }
+
+        return nullptr;
     }
 
     // Getter for the head of the Appointment list
@@ -256,26 +192,9 @@ bool isAppointmentIdExists(const AppointmentLL& appointmentList, int id) {
     return false;
 }
 
-// Function to check if an appointment is possible in the Appointment List
-bool isAppointmentNotValid(const AppointmentLL& appointmentList, int id, int doctor_id, int patient_id) {
-    Appointment* current = appointmentList.getHead();
-    while (current != nullptr) {
-        if (current->appointment_id == id)
-            return true;
-        if (current->doctor_id == doctor_id && current->patient_id == patient_id)
-            return true;
-        current = current->next;
-    }
-    return false;
-}
-
 // Function to book an appointment
-void registerAppointment(PatientLL& patientLl, DoctorLL& doctorLl, AppointmentLL& appointmentLl, int appointment_id, int patient_id, int doctor_id, string appointment_date) {
-    Patient* patient = patientLl.findPatient(patient_id);
-    Doctor* doctor = doctorLl.findDoctor(doctor_id);
-    Appointment* appointment = appointmentLl.findAppointment(appointment_id);
-
-    if (patient == nullptr && doctor == nullptr && appointment == nullptr) {
+void registerAppointment(AppointmentLL& appointmentLl, int appointment_id, int patient_id, int doctor_id, string appointment_date) {
+    if (appointmentLl.findAppointmentByDate(patient_id, doctor_id, appointment_date) == nullptr) {
         appointmentLl.addAppointment(appointment_id, patient_id, doctor_id, appointment_date);
     } else {
         cout << "Appointment already exists" << endl;
@@ -293,13 +212,17 @@ bool isValidIntegerInput() {
 }
 
 // Function to validate the date
-bool valid_date(string year,string month,string day){
-    int y = stoi(year);
-    int m = stoi(month);
-    int d = stoi(day);
-    if (isdigit(y) || isdigit(m) || isdigit(d)) {
+bool valid_date(string dateString){
+    // Check for valid format (dd/mm/yyyy)
+    if (dateString.length() != 10 || dateString[2] != '/' || dateString[5] != '/') {
         return false;
     }
+    // Extract day, month, and year from string
+    stringstream ss(dateString);
+    int d, m, y;
+    char ch;
+
+    ss >> d >> ch >> m >> ch >> y;
     unsigned short monthLen[]={31,28,31,30,31,30,31,31,30,31,30,31};
     if (!y || !m || !d || m>12)
         return false;
@@ -315,14 +238,6 @@ bool isValidStringInput(const string& input) {
     return !input.empty() && !all_of(input.begin(), input.end(), ::isspace);
 }
 
-bool checkGenderValue(string gender) {
-    if (gender != "Male" || gender != "Female") {
-        return false;
-    }
-    return true;
-}
-
-
 // Function to display the welcome message
 void displayWelcomeMessage() {
     cout << " =============================================================================================== " << endl;
@@ -331,6 +246,14 @@ void displayWelcomeMessage() {
     cout << "|                                                                                                |" << endl;
     cout << "|                                  Credits to BIRAMAHIRE Dan Bellamy.                            |" << endl;
     cout << " ================================================================================================ " << endl;
+}
+
+bool validateGender(const std::string& genderStr) {
+    // Convert the string to lowercase for case-insensitive comparison
+    string gender = genderStr;
+    transform(gender.begin(), gender.end(), gender.begin(), ::tolower);
+
+    return gender == "male" || gender == "female";
 }
 
 int main() {
@@ -361,13 +284,12 @@ int main() {
             case 1: {
                 cout << "PATIENT REGISTRATION" << endl;
                 cout << "--------------------" << endl;
+
                 int id;
                 string name;
                 string dob;
                 string gender;
                 bool isValidId = false;
-                bool isValidDate = false;
-                bool isValidGender = false;
 
                 while (!isValidId) {
                     cout << "ID: ";
@@ -381,7 +303,8 @@ int main() {
                     }
                     isValidId = true;
                 }
-patientNameVal:
+
+                patientNameVal:
                 cout << "NAME: ";
                 cin.ignore();
                 getline(cin, name);
@@ -391,37 +314,24 @@ patientNameVal:
                     goto patientNameVal;
                 }
 
-                vector<string> date;
-doBVal:
+                doBVal:
                 cout << "DoB: ";
-                cin.ignore();
                 getline(cin, dob);
 
-                if (!isValidStringInput(dob)) {
-                    cout << "Invalid date. Please enter a non-empty date.\n";
+                if (!valid_date(dob)) {
+                    cout << "Invalid date. Please provide a valid date (DD/MM/YYYY).\n";
                     goto doBVal;
                 }
-//                stringstream ss(dob);
-//                while (ss.good()) {
-//                    string substr;
-//                    getline(ss, substr, '/');
-//                    date.push_back(substr);
-//                }
-//
-//                if (!valid_date(date[2], date[1], date[0])) {
-//                    cout << "Invalid date. Please enter a non-empty date.\n";
-//                    goto doBVal;
-//                }
 
-genderVal:
+                genderVal:
                 cout << "GENDER: ";
-                cin.ignore();
                 getline(cin, gender);
 
-                if (!isValidStringInput(gender)) {
-                    cout << "Invalid date. Please enter a non-empty date.\n";
+                if (!validateGender(gender)) {
+                    cout << "Invalid gender. Please enter 'Male' or 'Female'.\n";
                     goto genderVal;
                 }
+
                 patientLl.addPatient(id, name, dob, gender);
                 break;
             }
@@ -444,7 +354,7 @@ genderVal:
                     }
                     isValidId = true;
                 }
-doctorNameVal:
+                doctorNameVal:
                 cout << "NAME: ";
                 cin.ignore();
                 getline(cin, name);
@@ -453,9 +363,8 @@ doctorNameVal:
                     cout << "Invalid name. Please provide a name.\n";
                     goto doctorNameVal;
                 }
-doctorSpecificationVal:
+                doctorSpecificationVal:
                 cout << "SPECIFICATION: ";
-                cin.ignore();
                 getline(cin, specialization);
 
                 if (!isValidStringInput(specialization)) {
@@ -495,7 +404,7 @@ doctorSpecificationVal:
                         cout << "Invalid ID. Please enter a positive integer.\n";
                         continue;
                     } else if (!isDoctorIdExists(doctorLl, doctor_id)) {
-                        cout << "Doctor with ID " << id << "not found.\n";
+                        cout << "Doctor with ID " << id << " not found.\n";
                         continue;
                     }
                     isValidDID = true;
@@ -508,36 +417,23 @@ doctorSpecificationVal:
                         cout << "Invalid ID. Please enter a positive integer.\n";
                         continue;
                     } else if (!isPatientIdExists(patientLl, patient_id)) {
-                        cout << "Patient with ID " << id << "not found.\n";
+                        cout << "Patient with ID " << id << " not found.\n";
                         continue;
                     }
                     isValidPID = true;
                 }
 
-                vector<string> date;
-aDate:
+                aDate:
                 cout << "DATE: ";
-                cin.ignore();
                 getline(cin, appointment_date);
 
-                if (!isValidStringInput(appointment_date)) {
-                    cout << "Invalid date. Please enter a non-empty date.\n";
+                if (!valid_date(appointment_date)) {
+                    cout << "Invalid date. Please provide a valid date (DD/MM/YYYY).\n";
                     goto aDate;
                 }
-//                stringstream ss(dob);
-//                while (ss.good()) {
-//                    string substr;
-//                    getline(ss, substr, '/');
-//                    date.push_back(substr);
-//                }
-//
-//                if (!valid_date(date[2], date[1], date[0])) {
-//                    cout << "Invalid date. Please enter a non-empty date.\n";
-//                    goto doBVal;
-//                }
 
 //                appointmentLl.addAppointment(id, patient_id, doctor_id, appointment_date);
-                registerAppointment(patientLl, doctorLl, appointmentLl,id, patient_id, doctor_id, appointment_date);
+                registerAppointment(appointmentLl,id, patient_id, doctor_id, appointment_date);
                 break;
             }
             case 4: {
@@ -550,6 +446,10 @@ aDate:
             }
             case 6: {
                 appointmentLl.displayAppointments();
+                break;
+            }
+            case 7: {
+                cout << "Exiting..." << endl;
                 break;
             }
             default:
